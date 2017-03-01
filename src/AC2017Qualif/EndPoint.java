9a2aa5ac46c9744f6aa93a2eb662d421e71a2952
none <notvalid@notvalid.com>
2017-03-01 23:18:17 +0100

    Updated definition

--- src/AC2017Qualif/EndPoint.java
+++ src/AC2017Qualif/EndPoint.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 
 public class EndPoint {
 
-	
+		
 	public int LD;//the latency of serving a video request from the data center to this endpoint, in milliseconds
 	public int K;//the number of cache servers that this endpoint is connected to
 	
-	public ArrayList<Server> ServerList;
-	public ArrayList<Integer> Latency4ServerList;// Latency to all servers. Put Max.Integer for server not connected
+	//public ArrayList<Server> ServerList;// removed
+	public ArrayList<Integer> Latency4ServerList;// Latency to all servers. Put  Integer.MAX_VALUE for server not connected
 	public ArrayList<Request> RequestList;
-	public EndPoint(int lD, int k, ArrayList<Server> serverList, ArrayList<Integer> latency4ServerList) {
+	
+	public EndPoint(int lD, int k, ArrayList<Integer> latency4ServerList) {
 		super();
 		LD = lD;
 		K = k;
-		ServerList = serverList;
 		Latency4ServerList = latency4ServerList;
 	}
 	
