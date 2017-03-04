29c9d10c71d10ac97e84c7b2c3307c9681fe3a70
none <notvalid@notvalid.com>
2017-03-04 12:00:52 +0100

    UPdated smallChange per problem, can go to 30th place.
    Next, will need local search for optimisation

--- src/AC2017Qualif/FullProcess.java
+++ src/AC2017Qualif/FullProcess.java
@@ -12,10 +12,10 @@ public class FullProcess {
 	public static void main(String[] args) {
 	
 		boolean RELOAD = true;
-//		Common.InputFileName = "kittens.in";RELOAD = false;
-	Common.InputFileName = "videos_worth_spreading.in";RELOAD = false;
-//	Common.InputFileName = "trending_today.in";RELOAD = false;//
-//		Common.InputFileName = "me_at_the_zoo.in";RELOAD = false;
+	//	Common.InputFileName = "kittens.in";Problem.smallOffset = 100;RELOAD = false;//100=> 1024782;  70 => 1024619   50 => 1024364   0 => 1021680
+	//Common.InputFileName = "videos_worth_spreading.in";Problem.smallOffset = 20;RELOAD = false;//20 => 608442
+	//Common.InputFileName = "trending_today.in";Problem.smallOffset = 20;RELOAD = false;//20 => 499984
+		Common.InputFileName = "me_at_the_zoo.in";Problem.smallOffset = 10;RELOAD = false;//10 => 507906
 //		Common.InputFileName = "example.in";RELOAD = false;
 		
 		Common.InputFilePath = Common.ACFileFolderPath+Common.InputFileName;
