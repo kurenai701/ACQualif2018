0445639b28226d268450b82948078da29d255d91
Clemence <Clemence@P34v2-CLEM>
2017-03-04 22:50:31 +0100

    commit in local output back to sol and adaptation of full process

--- src/AC2017Qualif/FullProcess.java
+++ src/AC2017Qualif/FullProcess.java
@@ -5,171 +5,184 @@ import java.util.SplittableRandom;
 import java.util.concurrent.TimeUnit;
 
 public class FullProcess {
-	
+
 	/**
-	 * @param args Main function
+	 * @param args
+	 *            Main function
 	 */
 	public static void main(String[] args) {
-	
+
 		boolean RELOAD = true;
-		//Common.InputFileName = "kittens.in";Problem.smallOffset = 100;RELOAD = false;//100=> 1024782;  70 => 1024619   50 => 1024364   0 => 1021680
-//	Common.InputFileName = "videos_worth_spreading.in";Problem.smallOffset = 20;RELOAD = false;//20 => 608442
-//	Common.InputFileName = "trending_today.in";Problem.smallOffset = 20;RELOAD = false;//20 => 499984
-		Common.InputFileName = "me_at_the_zoo.in";Problem.smallOffset = 10;RELOAD = false;//10 => 507906
-//		Common.InputFileName = "example.in";RELOAD = false;
-		
-		Common.InputFilePath = Common.ACFileFolderPath+Common.InputFileName;
+		// Common.InputFileName = "kittens.in";Problem.smallOffset = 100;RELOAD
+		// = false;//100=> 1024782; 70 => 1024619 50 => 1024364 0 => 1021680
+		// Common.OutputGeneratedFileName = "kittens.out.txt";
+		// Common.InputFileName =
+		// "videos_worth_spreading.in";Problem.smallOffset = 20;RELOAD =
+		// false;//20 => 608442
+		// Common.OutputGeneratedFileName = "videos_worth_spreading.out.txt";
+		// Common.InputFileName = "trending_today.in";Problem.smallOffset =
+		// 20;RELOAD = false;//20 => 499984
+		// Common.OutputGeneratedFileName = "trending_today.out.txt";
+
+		Common.InputFileName = "me_at_the_zoo.in";
+		Problem.smallOffset = 10;
+		RELOAD = false;// 10 => 507906
+		Common.OutputGeneratedFileName = "me_at_the_zoo.out.txt";
+
+		// Common.InputFileName = "example.in";RELOAD = false;
+
+		Common.InputFilePath = Common.ACFileFolderPath + Common.InputFileName;
+		Common.OutputGeneratedFullPath = Common.ACFileFolderPath + Common.OutputGeneratedFileName;
+
 		// PARAMETRES !!
 
-	
-		
-		
-		
-		//// Initialization of our Tool Classes		
-		AlgoInputToOutput Alg = null;//new AlgoInputToOutput();
-				
+		RELOAD = true;
+
+		//// Initialization of our Tool Classes
+		AlgoInputToOutput Alg = null;// new AlgoInputToOutput();
+
 		// ****** Get Problem Model from file
-		
+
 		ReadInput ri = new ReadInput();
 		Scanner scanInput = ri.ScannerInputFile(Common.InputFilePath);
 		Problem pbMod = ri.ProcessReadInputToModel(scanInput);
-		
 
 		// TODO : find solution
-		
-		
-		
+
 		// ****** Process Algorithm to find Solution
-		SplittableRandom rand = new SplittableRandom(System.currentTimeMillis() );
+		SplittableRandom rand = new SplittableRandom(System.currentTimeMillis());
 		Solution sol;
-		
-		if(!RELOAD)
-		{
+
+		if (!RELOAD) {
 			Solution startSol = new Solution(pbMod);
-			MutableDouble BestScoreOut= new MutableDouble(0);
+			MutableDouble BestScoreOut = new MutableDouble(0);
 			BestSolutionSynchro BestSolSynch = new BestSolutionSynchro(startSol);
-			Alg = new AlgoInputToOutput(pbMod, rand,startSol,BestSolSynch,0,50);
-								
-			sol =  Alg.AlgoInit( pbMod,  rand);
-			
-			
-			//return;//TODO
-			
+			Alg = new AlgoInputToOutput(pbMod, rand, startSol, BestSolSynch, 0, 50);
+
+			sol = Alg.AlgoInit(pbMod, rand);
+
+			// return;//TODO
+
 			ProcessAllBackupOfSolutionToFolder(sol);
-		}else
-		{
-			 sol = (Solution)(Common.FU.DeserializeFileToObject(Common.ACFileFolderPath + Common.InputFileName + "_BestSolutionInProcess.ser"));//0000671212_0330-2224\\SolutionSerialized.ser" ));
-			
-			//sol.pb = pbMod;
+		} else {
+			// sol =
+			// (Solution)(Common.FU.DeserializeFileToObject(Common.ACFileFolderPath
+			// + Common.InputFileName +
+			// "_BestSolutionInProcess.ser"));//0000671212_0330-2224\\SolutionSerialized.ser"
+			// ));
+
+			// REPROCESS INIT PROBLEM
+			Scanner scanIn = ri.ScannerInputFile(Common.InputFilePath);
+			pbMod = ri.ProcessReadInputToModel(scanIn);
+
+			// GET LATEST SOLUTION FROM OUTPUT
+			OutputBackToSol outputToSol = new OutputBackToSol();
+			Scanner scanOutput = outputToSol.ScannerOutputFile(Common.OutputGeneratedFullPath);
+			sol = outputToSol.ProcessReadOutputBackToSolution(scanOutput, pbMod);
+
+			Sys.disp("recreated solution from output");
+			// sol.pb = pbMod;
 		}
-		
+
 		// Todo : do DeepCopy if needed
-		
-		
+
 		Sys.disp(sol.GetScore());
-	
+
 		// ****** Solution Improver ATTENTION AUX PARAMETRES;
 		BestSolutionSynchro BestSolSynch = new BestSolutionSynchro(sol);
-	while(true)
-	{
-		int NIT = 5;
-		
-		// ******************* Parallel Threads ********************
-		int NPROC = 1;
-		
-		Thread[] thL = new Thread[NPROC];
-		for(int i =0;i<NPROC;i++)
-		{
-			SplittableRandom indivrand = new SplittableRandom(rand.nextInt() );
-			Solution indivSol = BestSolSynch.gestBestSolution();
-			
-			indivSol.pb = pbMod;
+		while (true) {
+			int NIT = 5;
 
-		
-			Runnable indivAlg = new AlgoInputToOutput(pbMod, indivrand,indivSol,BestSolSynch, i,NIT);
-			Thread thread = new Thread(indivAlg);
-			thread.start();
-			thL[i]=  thread;
-			
-		}
-		boolean finished = false;
-			while(!finished)
-			{
-				finished=true;
+			// ******************* Parallel Threads ********************
+			int NPROC = 1;
+
+			Thread[] thL = new Thread[NPROC];
+			for (int i = 0; i < NPROC; i++) {
+				SplittableRandom indivrand = new SplittableRandom(rand.nextInt());
+				Solution indivSol = BestSolSynch.gestBestSolution();
+
+				indivSol.pb = pbMod;
+
+				Runnable indivAlg = new AlgoInputToOutput(pbMod, indivrand, indivSol, BestSolSynch, i, NIT);
+				Thread thread = new Thread(indivAlg);
+				thread.start();
+				thL[i] = thread;
+
+			}
+			boolean finished = false;
+			while (!finished) {
+				finished = true;
 				try {
 					TimeUnit.SECONDS.sleep(1);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				for(Thread t : thL)
-				{
-					if(t.isAlive())
-					{
+
+				for (Thread t : thL) {
+					if (t.isAlive()) {
 						finished = false;
 					}
-					
+
 				}
-			
+
 			}
 		}
 
-	
-	}	
+	}
 
-	
-	
-	public static void VerifySerializeDeserializeSolution(Solution sol)
-	{
+	/*
+	public static void VerifySerializeDeserializeSolution(Solution sol) {
 		//// Serialize Solution class
 		sol.SaveSolutionAsRaw(Common.SaveSerialFileName);
-		
-		//// De-serialize to verify there is no problem	
-		Solution testdes = (Solution)(Common.FU.DeserializeFileToObject(Common.ACFileFolderPath+Common.SaveSerialFileName));
-	
+
+		//// De-serialize to verify there is no problem
+		Solution testdes = (Solution) (Common.FU
+				.DeserializeFileToObject(Common.ACFileFolderPath + Common.SaveSerialFileName));
+
 	}
-	
-	public static String getDirectoryNameWithScoreAndDate(int scorePrefix, String prefixDate)
-	{			
-		String resPath = Common.InputFileName+ "_"+ String.format("%010d", scorePrefix);//+ "_" + prefixDate;
+	*/
+
+	public static String getDirectoryNameWithScoreAndDate(int scorePrefix, String prefixDate) {
+		String resPath = Common.InputFileName + "_" + String.format("%010d", scorePrefix);// +
+																							// "_"
+																							// +
+																							// prefixDate;
 		return resPath;
 	}
-	
-	
-	public static void ProcessAllBackupOfSolutionToFolder(Solution sol)
-	{		
+
+	public static void ProcessAllBackupOfSolutionToFolder(Solution sol) {
 		// CheckSolution
-		CheckSolution(sol);
-		
-		
+		// CheckSolution(sol);
+
 		// Initialize Tool
 		GenerateOutput genOut = new GenerateOutput();
-		
+
 		////////// FOR BACK-UP ////////////
-		String datePrefix = Common.getTimeAsPrefixDateFormatString();			
-		String TargetBKPfolderName = getDirectoryNameWithScoreAndDate((int)Math.round(sol.GetScore()),datePrefix);
-	
-		String targetBKPFolderPath = Common.ACFileFolderPath+TargetBKPfolderName;
+		String datePrefix = Common.getTimeAsPrefixDateFormatString();
+		String TargetBKPfolderName = getDirectoryNameWithScoreAndDate((int) Math.round(sol.GetScore()), datePrefix);
+
+		String targetBKPFolderPath = Common.ACFileFolderPath + TargetBKPfolderName;
 		Common.FU.CreateDir(targetBKPFolderPath);
-	
+
 		// Backup serialized solution class to BKP folder
-////		sol.SaveSolutionAsRawToFullPath(targetBKPFolderPath+"\\"+ Common.SaveSerialFileName);
-		
+		//// sol.SaveSolutionAsRawToFullPath(targetBKPFolderPath+"\\"+
+		//// Common.SaveSerialFileName);
+
 		// Backup generated output
-		genOut.GenerateOutputFileFromOutputModel(sol, targetBKPFolderPath +"\\"+ Common.OutputGeneratedFileName);
-	
-		// Zip Sources for BKP	
+		genOut.GenerateOutputFileFromOutputModel(sol, targetBKPFolderPath + "\\" + Common.OutputGeneratedFileName);
+
+		// same output file in main PROBLEM folder
+		genOut.GenerateOutputFileFromOutputModel(sol, Common.OutputGeneratedFullPath);
+
+		// Zip Sources for BKP
 		UtilSrcZip appZip = new UtilSrcZip();
-		appZip.ZipSourceOfProject(targetBKPFolderPath+"\\"+Common.OUTPUT_ZIP_FULLPROC_FILE_NAME);	
+		appZip.ZipSourceOfProject(targetBKPFolderPath + "\\" + Common.OUTPUT_ZIP_FULLPROC_FILE_NAME);
 	}
-	
-	public static void CheckSolution(Solution sol)
-	{
-	
-	
-	//		assert( (NT>=sol.pb.L && NM>=sol.pb.L));
-			
-		
+
+	public static void CheckSolution(Solution sol) {
+
+		// assert( (NT>=sol.pb.L && NM>=sol.pb.L));
+
 	}
 }
---
+++ src/AC2017Qualif/OutputBackToSol.java
@@ -0,0 +1,82 @@
+package AC2017Qualif;
+
+import java.util.Scanner;
+
+public class OutputBackToSol {
+
+	public Scanner ScannerOutputFile(String fileFullPath) {
+		return Common.FU.ScannerFile(fileFullPath);
+	}
+
+	// From Output file back to Solution Model
+	// Problem should be already recreated through ReadInput
+	public Solution ProcessReadOutputBackToSolution(Scanner scOut, Problem pb) {
+		Sys.disp("ProcessReadOutputBackToSolution");
+		Solution sol = new Solution(pb);
+
+		/////////////// Beginning TAKEN FROM AlgoINIT //////
+		
+		// First, update all server priority list
+		for(Video vid : pb.VideoList)
+		{
+			for(Server s : pb.ServerList)
+			{
+				VideoGain VG = new VideoGain(vid, -1.0);
+				s.AllVideoGains.add(VG);
+				s.VideosPriority.add(VG);
+				
+			}			
+		}
+		/////////////////////////
+		
+		
+		
+		if (scOut != null) {
+			// In the output file that I generate, even if a server is not used.
+			// There is a line for that server with the id. But no Video
+			
+			// Getting rid of the first line
+			String line1 = scOut.nextLine();
+			Sys.disp("line1 : " + line1);
+			
+			for (int i = 0; i < pb.C; i++) {
+				String line = scOut.nextLine();
+				Sys.disp(line);
+				String[] splitline = line.split(" ");
+	
+				int idServer = Integer.parseInt(splitline[0]);
+				Sys.disp("idServer" + idServer);
+				Server curServer = pb.ServerList.get(idServer);
+				for (int j = 1; j < splitline.length; j++) {
+					int idVid = Integer.parseInt(splitline[j]);
+					Sys.disp("idVid" + idVid);
+					Video curVid = pb.VideoList.get(idVid);
+                    
+					
+					curServer.PutVideoInCache(curVid);
+				}
+			}
+			
+		    // Videos have been assigned back to their server
+		    // Now reupdate video gains
+			
+			int cntvid = 0;
+			for(Video vid : pb.VideoList)
+			{
+				pb.ServerList.get(0).updateAllServersVG(vid);
+				cntvid++;
+				if(cntvid%100==0)
+				{
+					Sys.disp("vid add : " + vid.ID);
+				}
+			}
+
+		
+		}
+		
+		
+		Sys.disp("DONE GETTING SOL BACK FROM OUTPUT");
+		return sol;
+	}
+
+}
